16d992582d76f9d4a5ea0fb711f3d2ecc750e5c2
ClanEver <562211524@qq.com>
2024-12-25 14:21:11 +0800

    keymap

--- components/CollaborativeEditor.tsx
+++ components/CollaborativeEditor.tsx
@@ -1,11 +1,15 @@
-"use client";
+'use client'
 
-import { useEffect, useRef, useState } from "react";
-import { io, Socket } from "socket.io-client";
-import CodeMirror, { keymap } from "@uiw/react-codemirror";
-import { StreamLanguage } from "@codemirror/language";
+import { useEffect, useRef, useState } from 'react'
+import { io, Socket } from 'socket.io-client'
+import CodeMirror, {
+    EditorView,
+    keymap,
+    ReactCodeMirrorRef,
+} from '@uiw/react-codemirror'
+import { StreamLanguage } from '@codemirror/language'
 // import { vscodeDark } from '@uiw/codemirror-theme-vscode'
-import { python } from "@codemirror/legacy-modes/mode/python";
+import { python } from '@codemirror/legacy-modes/mode/python'
 import {
     c,
     cpp,
@@ -14,121 +18,135 @@ import {
     kotlin,
     java,
     scala,
-} from "@codemirror/legacy-modes/mode/clike";
+} from '@codemirror/legacy-modes/mode/clike'
 import {
     javascript,
     typescript,
-} from "@codemirror/legacy-modes/mode/javascript";
-import { php } from "@codemirror/lang-php";
-import { go } from "@codemirror/legacy-modes/mode/go";
-import { ruby } from "@codemirror/legacy-modes/mode/ruby";
-import { rust } from "@codemirror/legacy-modes/mode/rust";
-import { erlang } from "@codemirror/legacy-modes/mode/erlang";
-import { swift } from "@codemirror/legacy-modes/mode/swift";
-import { scheme } from "@codemirror/legacy-modes/mode/scheme";
-import { elixir } from "codemirror-lang-elixir";
+} from '@codemirror/legacy-modes/mode/javascript'
+import { php } from '@codemirror/lang-php'
+import { go } from '@codemirror/legacy-modes/mode/go'
+import { ruby } from '@codemirror/legacy-modes/mode/ruby'
+import { rust } from '@codemirror/legacy-modes/mode/rust'
+import { erlang } from '@codemirror/legacy-modes/mode/erlang'
+import { swift } from '@codemirror/legacy-modes/mode/swift'
+import { scheme } from '@codemirror/legacy-modes/mode/scheme'
+import { elixir } from 'codemirror-lang-elixir'
+import { insertNewlineAndIndent } from '@codemirror/commands'
 
 interface CollaborativeEditorProps {
-    roomId: string | undefined;
+    roomId: string | undefined
 }
 
 const languages = [
-    { name: "Python", extension: () => StreamLanguage.define(python) },
-    { name: "C#", extension: () => StreamLanguage.define(csharp) },
-    { name: "C++", extension: () => StreamLanguage.define(cpp) },
-    { name: "C", extension: () => StreamLanguage.define(c) },
-    { name: "JavaScript", extension: () => StreamLanguage.define(javascript) },
-    { name: "TypeScript", extension: () => StreamLanguage.define(typescript) },
-    { name: "Kotlin", extension: () => StreamLanguage.define(kotlin) },
-    { name: "Java", extension: () => StreamLanguage.define(java) },
-    { name: "PHP", extension: () => php() },
-    { name: "Swift", extension: () => StreamLanguage.define(swift) },
-    { name: "Dart", extension: () => StreamLanguage.define(dart) },
-    { name: "Go", extension: () => StreamLanguage.define(go) },
-    { name: "Ruby", extension: () => StreamLanguage.define(ruby) },
-    { name: "Scala", extension: () => StreamLanguage.define(scala) },
-    { name: "Rust", extension: () => StreamLanguage.define(rust) },
-    { name: "Racket", extension: () => StreamLanguage.define(scheme) },
-    { name: "Erlang", extension: () => StreamLanguage.define(erlang) },
-    { name: "Elixir", extension: () => elixir() },
-];
+    { name: 'Python', extension: () => StreamLanguage.define(python) },
+    { name: 'C#', extension: () => StreamLanguage.define(csharp) },
+    { name: 'C++', extension: () => StreamLanguage.define(cpp) },
+    { name: 'C', extension: () => StreamLanguage.define(c) },
+    { name: 'JavaScript', extension: () => StreamLanguage.define(javascript) },
+    { name: 'TypeScript', extension: () => StreamLanguage.define(typescript) },
+    { name: 'Kotlin', extension: () => StreamLanguage.define(kotlin) },
+    { name: 'Java', extension: () => StreamLanguage.define(java) },
+    { name: 'PHP', extension: () => php() },
+    { name: 'Swift', extension: () => StreamLanguage.define(swift) },
+    { name: 'Dart', extension: () => StreamLanguage.define(dart) },
+    { name: 'Go', extension: () => StreamLanguage.define(go) },
+    { name: 'Ruby', extension: () => StreamLanguage.define(ruby) },
+    { name: 'Scala', extension: () => StreamLanguage.define(scala) },
+    { name: 'Rust', extension: () => StreamLanguage.define(rust) },
+    { name: 'Racket', extension: () => StreamLanguage.define(scheme) },
+    { name: 'Erlang', extension: () => StreamLanguage.define(erlang) },
+    { name: 'Elixir', extension: () => elixir() },
+]
 
 export default function CollaborativeEditor({
     roomId,
 }: CollaborativeEditorProps) {
-    const [editorContent, setEditorContent] = useState("");
-    const [isReadOnly, setIsReadOnly] = useState(true);
-    const [userId, setUserId] = useState<number | null>(null);
-    const [editingUser, setEditingUser] = useState<number | null>(null);
-    const [isJoined, setIsJoined] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-    const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
-    const socketRef = useRef<Socket | null>(null);
+    const [editorContent, setEditorContent] = useState('')
+    const [isReadOnly, setIsReadOnly] = useState(false)
+    const [userId, setUserId] = useState<number | null>(null)
+    const [editingUser, setEditingUser] = useState<number | null>(null)
+    const [isJoined, setIsJoined] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+    const [selectedLanguage, setSelectedLanguage] = useState(languages[0])
+    const socketRef = useRef<Socket>(null)
+    const editorRef = useRef<ReactCodeMirrorRef>(null)
 
     useEffect(() => {
         if (!roomId) {
-            setError("Invalid room ID");
-            return;
+            setError('Invalid room ID')
+            return
         }
 
-        socketRef.current = io();
+        socketRef.current = io()
 
-        socketRef.current.on("connect", () => {
-            console.log("Connected to server");
-            socketRef.current?.emit("joinRoom", roomId);
-        });
+        socketRef.current.on('connect', () => {
+            console.log('Connected to server')
+            socketRef.current?.emit('joinRoom', roomId)
+        })
 
-        socketRef.current.on("userId", (id: number) => {
-            setUserId(id);
-            setIsJoined(true);
-        });
+        socketRef.current.on('userId', (id: number) => {
+            setUserId(id)
+            setIsJoined(true)
+        })
 
-        socketRef.current.on("roomFull", () => {
-            setError("Room is full. Please try another room.");
-        });
+        socketRef.current.on('roomFull', () => {
+            setError('Room is full. Please try another room.')
+        })
 
-        socketRef.current.on("initialContent", (content: string) => {
-            setEditorContent(content);
-        });
+        socketRef.current.on('initialContent', (content: string) => {
+            setEditorContent(content)
+        })
 
-        socketRef.current.on("contentChange", (newContent: string) => {
-            setEditorContent(newContent);
-        });
+        socketRef.current.on('contentChange', (newContent: string) => {
+            setEditorContent(newContent)
+        })
 
-        socketRef.current.on("setEditingUser", (editingUserId: number) => {
-            setEditingUser(editingUserId);
-            setIsReadOnly(editingUserId !== userId);
-        });
+        socketRef.current.on('setEditingUser', (editingUserId: number) => {
+            setEditingUser(editingUserId)
+            // setIsReadOnly(editingUserId !== userId)
+        })
 
         return () => {
-            socketRef.current?.disconnect();
-        };
-    }, [roomId, userId]);
+            socketRef.current?.disconnect()
+        }
+    }, [roomId, userId])
+
+    // 强制光标在末尾
+    useEffect(() => {
+        if (!editorRef.current) return
+        const editor = editorRef.current?.view
+        editor?.dispatch({
+            selection: {
+                anchor: editor.state.doc.length,
+                head: editor.state.doc.length,
+            },
+        })
+    }, [editorContent])
 
     const handleEditorChange = (value: string) => {
         if (roomId && !isReadOnly) {
-            setEditorContent(value);
-            const lastChar = value.slice(-1);
-            socketRef.current?.emit("contentChange", {
+            setEditorContent(value)
+            const lastChar = value.slice(-1)
+            socketRef.current?.emit('contentChange', {
                 roomId,
                 content: value,
                 lastChar,
-            });
+            })
         }
-    };
+    }
 
     const requestEditPermission = () => {
         if (roomId) {
-            socketRef.current?.emit("requestEditPermission", roomId);
+            socketRef.current?.emit('requestEditPermission', roomId)
         }
-    };
+    }
 
     if (!roomId) {
         return (
             <div className="flex items-center justify-center h-screen">
                 <p className="text-xl text-red-500">Invalid room ID</p>
             </div>
-        );
+        )
     }
 
     if (!isJoined) {
@@ -136,7 +154,7 @@ export default function CollaborativeEditor({
             <div className="flex items-center justify-center h-screen">
                 <p className="text-xl">Joining room {roomId}...</p>
             </div>
-        );
+        )
     }
 
     if (error) {
@@ -144,47 +162,42 @@ export default function CollaborativeEditor({
             <div className="flex items-center justify-center h-screen">
                 <p className="text-xl text-red-500">{error}</p>
             </div>
-        );
+        )
     }
 
-    const customKeymap = keymap.of([
-        {
-            key: "Ctrl-c",
-            run: (view) => {
-                const selection = view.state.selection.main;
-                const text = view.state.doc.sliceString(
-                    selection.from,
-                    selection.to,
-                );
-                navigator.clipboard.writeText(text);
-                return true;
+    const extensions = [
+        selectedLanguage.extension(),
+        EditorView.domEventHandlers({
+            // 禁止粘贴
+            paste: (e) => {
+                e.preventDefault()
+                return true
             },
-        },
-        {
-            key: "Cmd-c",
-            run: (view) => {
-                const selection = view.state.selection.main;
-                const text = view.state.doc.sliceString(
-                    selection.from,
-                    selection.to,
-                );
-                navigator.clipboard.writeText(text);
-                return true;
-            },
-        },
-        {
-            key: "Enter",
-            run: (view) => {
-                if (roomId && !isReadOnly) {
-                    socketRef.current?.emit("requestEditPermission", roomId);
+            // 拦截方向键、Home、End等光标移动按键
+            keydown: (e) => {
+                if (
+                    e.key.includes('Arrow') ||
+                    e.key === 'Home' ||
+                    e.key === 'End' ||
+                    e.key === 'PageUp' ||
+                    e.key === 'PageDown'
+                ) {
+                    e.preventDefault()
+                    return true
                 }
-                return true;
+                return false
             },
-        },
-    ]);
+            // 禁用鼠标点击移动光标
+            mousedown: (e) => {
+                e.preventDefault()
+                return true
+            },
+        }),
+        keymap.of([{ key: 'Enter', run: insertNewlineAndIndent }]),
+    ]
 
     return (
-        <div className="w-full h-[600px]">
+        <div className="w-full h-[600px]" onPaste={(e) => e.preventDefault()}>
             <div className="mb-4">
                 <select
                     value={selectedLanguage.name}
@@ -205,10 +218,11 @@ export default function CollaborativeEditor({
                 </select>
             </div>
             <CodeMirror
+                ref={editorRef}
                 value={editorContent}
                 height="100%"
                 // theme={vscodeDark}
-                extensions={[selectedLanguage.extension(), customKeymap]}
+                extensions={extensions}
                 onChange={handleEditorChange}
                 editable={!isReadOnly}
                 basicSetup={{
@@ -235,7 +249,7 @@ export default function CollaborativeEditor({
                     You are User {userId} in Room {roomId}.
                     {isReadOnly
                         ? `User ${editingUser} is currently editing.`
-                        : "You can edit now. Press Enter to switch control."}
+                        : 'You can edit now. Press Enter to switch control.'}
                 </p>
                 {isReadOnly && (
                     <button
@@ -247,5 +261,5 @@ export default function CollaborativeEditor({
                 )}
             </div>
         </div>
-    );
+    )
 }
